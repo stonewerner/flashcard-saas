bc6aae643a1d56570f47b534a3cc6cfc64d6a0c4
Stone Werner <stonewerner@berkeley.edu>
2024-08-16 10:59:36 -0700

    features and pricing 35:46

--- app/page.js
+++ app/page.js
@@ -26,12 +26,44 @@ export default function Home() {
         <Button variant="contained" color="primary" sx={{mt: 2}}>Get Started</Button>
       </Box>
       <Box sx={{my: 6}}>
-        <Typography variant="h4" components="h2">Features</Typography>
-        <Grid contained spacing={4}>
-          <Grid item xs={12} md={4}></Grid>
-
+        <Typography variant="h4">Features</Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Smart Flash Cards</Typography>
+            <Typography>Upload a pdf and our AI will make intuitive flashcards!</Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Accessible Anwhere</Typography>
+            <Typography>Access your flashcards from any device!</Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6">Easy Text Input</Typography>
+            <Typography>Just Input your text and let our AI do the rest. Studying has never been easier!</Typography>
+          </Grid>
         </Grid>
-
+      </Box>
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4">Pricing</Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Box sx={{p: 3, border: '1px solid', borderColor: 'grey.300', borderRadius: 2,}}>
+              <Typography variant="h6">Rook</Typography>
+              <Typography>Upload a pdf and our AI will make intuitive flashcards!</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={{p: 3, border: '1px solid', borderColor: 'grey.300', borderRadius: 2,}}>
+              <Typography variant="h6">Intern</Typography>
+              <Typography>Access your flashcards from any device!</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={{p: 3, border: '1px solid', borderColor: 'grey.300', borderRadius: 2,}}>
+              <Typography variant="h6">Pro</Typography>
+              <Typography>Just Input your text and let our AI do the rest. Studying has never been easier!</Typography>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
