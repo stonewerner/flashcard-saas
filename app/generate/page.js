b11c487656b11593d590418c0e530a8e8a27f43d
Stone Werner <stonewerner@berkeley.edu>
2024-08-16 11:48:26 -0700

    generating flashcards 56:22

---
+++ app/generate/page.js
@@ -0,0 +1,73 @@
+'use client'
+import {useUser} from '@clerk/nextjs'
+import {useRouter} from 'next/navigation'
+
+
+export default function Generate() {
+    const {isLoaded, isSignedIn, user} = useUser()
+    const [flashcards, setFlashcards] = useState([])
+    const [flipped, setFlipped] = useState([])
+    const [text, setText] = useState('')
+    const [name, setName] = useState('')
+    const [open, setOpen] = useState(false)
+    const router = userRouter
+
+    const handleSubmit = async () => {
+        fetch('api/generate', {
+            method: 'POST',
+            body: text,
+        })
+        .then((res) => res.json())
+        .then(data > setFlashcards(data))
+    }
+
+    const handleCardClick = (id) => {
+        setFlipped((prev) => ({
+            ...prev,
+            [id]: !prev[id],
+        }))
+    }
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const saveFlashcards = async () => {
+        if (!name) {
+            alert('Please enter a name')
+            return
+        }
+
+        const batch = writeBatch(db)
+        const userDocRef = doc(collection(db, 'users'), user.id)
+        const docSnap = await getDoc(userDocRef)
+
+        if (docSnap.exists()) {
+            const collections = docSnap.data().flashcards || []
+            if (collections.find((f) => f.name === name)) {
+                alert("Flashcard collections with same name already exisits.")
+                return
+            } else {
+                collections.push({name})
+                batch.set(userDocRef, {flashcards: collections}, {merge: true})
+            }
+        } else {
+            batch.set(userDocRef, {flashcards: [{name}]})
+        }
+
+        const colRef = collection(userDocRef, name)
+        flashcards.forEach((flashcard) => {
+            const cardDocRef = doc(colRef)
+            batch.set(cardDocRef, flashcard)
+        })
+
+        await
+
+
+        
+    }
+}
