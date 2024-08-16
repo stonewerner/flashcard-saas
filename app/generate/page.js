6b20a1f41bb4337f306271be3d40addf791185e1
Stone Werner <stonewerner@berkeley.edu>
2024-08-16 12:59:24 -0700

    flashcard display and animation 1:06:25

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,6 +1,8 @@
 'use client'
 import {useUser} from '@clerk/nextjs'
 import {useRouter} from 'next/navigation'
+import { useState } from 'react'
+import { Container, Box, TextField, Paper, Typography, Button, Card, CardContent, CardActionArea} from '@mui/material'
 
 
 export default function Generate() {
@@ -10,7 +12,7 @@ export default function Generate() {
     const [text, setText] = useState('')
     const [name, setName] = useState('')
     const [open, setOpen] = useState(false)
-    const router = userRouter
+    const router = useRouter
 
     const handleSubmit = async () => {
         fetch('api/generate', {
@@ -65,9 +67,67 @@ export default function Generate() {
             batch.set(cardDocRef, flashcard)
         })
 
-        await
-
-
-        
+        await batch.commit()
+        handleClose()
+        router.push('/flashcards')
     }
+
+    return(
+        <Container maxWidth="md">
+            <Box sx={{
+                mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center'
+            }}>
+                <Typography variant="h4">Generate Flashcards</Typography>
+                <Paper sx={{p: 4, width: '100%'}}>
+                    <TextField value={text} onChange={(e) => setText(e.target.value)} label="Enter text" fullWidth multiline rows={4} variant='outlined' sx={{mb: 2,}}/>
+                    <Button variant='contained' color='primary' onClick={handleSubmit} fullWidth>Submit</Button>
+                </Paper>
+            </Box>
+
+            {flashcards.length > 0 && <Box sx={{mt: 4}}>
+                <Typography variant='h5' >
+                    Flashcards Preview
+                </Typography>
+                <Grid container spacing={3}>
+                    {flashcards.map((flashcard, index) => {
+                        <Grid item xs = {12} sm = {6} md = {4} key = {index}>
+                            <Card>
+                                <CardActionArea onClick={() => {
+                                    handleCardClick(index)
+                                }}>
+                                    <CardContent>
+                                        <Box sx = {{
+                                            perspective: '1000px',
+                                            '& > div': {
+                                                transition: 'transform 0.6s',
+                                                transformStyle: 'preserve-3d',
+                                                position: 'relative',
+                                                width: '100%',
+                                                height: '200px',
+                                                boxShadow: '0 4px 8px 0 rgba(0,0,0, 0.2)',
+                                                transform: flipped[index]? 'rotateY(180deg)' : 'rotateY(0deg)',
+                                            },
+                                        }}>
+                                            <div>
+                                                <div>
+                                                    <Typography variant="h5" component="div">{flashcard.front}</Typography>
+                                                </div>
+                                                <div>
+                                                    <Typography variant="h5" component="div">{flashcard.back}</Typography>
+                                                </div>
+                                            </div>
+                                        </Box>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+
+                    })}
+
+                </Grid>
+
+            </Box>}
+
+        </Container>
+    )
 }
